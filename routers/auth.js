b39b783407c2722ddb311595155d3fb481743925
WojciechLisCode <wlis58372+code@gmail.com>
2021-05-31 18:03:30 +0200

    user space added on login

--- routers/auth.js
+++ routers/auth.js
@@ -3,6 +3,7 @@ const { Router } = require("express");
 const { toJWT } = require("../auth/jwt");
 const authMiddleware = require("../auth/middleware");
 const User = require("../models/").user;
+const Space = require("../models/").space;
 const { SALT_ROUNDS } = require("../config/constants");
 
 const router = new Router();
@@ -21,7 +22,7 @@ router.post("/login", async (req, res, next) => {
 
     if (!user || !bcrypt.compareSync(password, user.password)) {
       return res.status(400).send({
-        message: "User with that email not found or password incorrect"
+        message: "User with that email not found or password incorrect",
       });
     }
 
@@ -44,14 +45,19 @@ router.post("/signup", async (req, res) => {
     const newUser = await User.create({
       email,
       password: bcrypt.hashSync(password, SALT_ROUNDS),
-      name
+      name,
     });
 
     delete newUser.dataValues["password"]; // don't send back the password hash
 
     const token = toJWT({ userId: newUser.id });
 
-    res.status(201).json({ token, ...newUser.dataValues });
+    const userSpace = await Space.create({
+      title: `${newUser.name}'s space`,
+      userId: newUser.id,
+    });
+
+    res.status(201).json({ token, ...newUser.dataValues, userSpace });
   } catch (error) {
     if (error.name === "SequelizeUniqueConstraintError") {
       return res
